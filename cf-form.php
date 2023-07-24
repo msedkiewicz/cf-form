74ca689c5f0d90c9b5b8d00f031d797a26085aba
msedkiewicz <magdalenainspirations@gmail.com>
2023-07-24 19:07:39 +0200

    Add hook for contact form frontend page

--- cf-form.php
+++ cf-form.php
@@ -33,6 +33,8 @@ if( !class_exists('CFFormMsedkiewicz')) {
             include_once CFFORM_PATH . 'includes/utilities.php';
 
             include_once CFFORM_PATH . 'includes/options-page.php';
+
+            include_once CFFORM_PATH . 'includes/contact-form.php';
         }
     }
 $cfForm = new CFFormMsedkiewicz;
