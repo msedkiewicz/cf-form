b8e71fae72fa4e71c1ba5feea4de3b04104964eb
msedkiewicz <magdalenainspirations@gmail.com>
2023-07-24 19:33:49 +0200

    Setting up REST endpoint

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -1,8 +1,25 @@
 <?php
 
 add_shortcode('contact', 'cfmsedkiewicz_show_contact_form');
+add_action('rest_api_init', 'cfmsedkiewicz_create_rest_endpoint');
 
 function cfmsedkiewicz_show_contact_form()
 {
     include CFFORM_PATH . '/includes/templates/contact-form.php';
+}
+
+function cfmsedkiewicz_create_rest_endpoint()
+{
+    $post = 'POST';
+    $callback = 'cfmsedkiewicz_handle_enquiry';
+
+    register_rest_route('v1/contact-form', 'submit', array(
+        'methods' -> $post,
+        'callback' -> $callback,
+    ));
+}
+
+function cfmsedkiewicz_handle_enquiry()
+{
+    echo 'hello';
 }
