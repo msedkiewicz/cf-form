7e7003f2bdc2607348c6dd7c7f260294f5217aec
msedkiewicz <magdalenainspirations@gmail.com>
2023-07-24 18:09:08 +0200

    Hook Carbon Fields

--- includes/options-page.php
+++ includes/options-page.php
@@ -1,1 +1,11 @@
 <?php
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+
+add_action('after_setup_theme', 'load_carbon_fields');
+
+function load_carbon_fields() {
+    \Carbon_Fields\Carbon_Fields::boot();
+}
