37a72cb1e5897d13b81f665b5fba2c27dfce3489
msedkiewicz <magdalenainspirations@gmail.com>
2023-07-24 18:28:17 +0200

    Setting up basic options page

--- includes/options-page.php
+++ includes/options-page.php
@@ -3,9 +3,17 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
+add_action('after_setup_theme', 'cfmsedkiewicz_load_carbon_fields');
+add_action('carbon_fields_register_fields', 'cfmsedkiewicz_create_options_page');
 
-add_action('after_setup_theme', 'load_carbon_fields');
-
-function load_carbon_fields() {
+function cfmsedkiewicz_load_carbon_fields() {
     \Carbon_Fields\Carbon_Fields::boot();
+}
+
+function cfmsedkiewicz_create_options_page() {
+    Container::make( 'theme_options', 'CF Form Options' )
+    ->add_fields( array(
+        Field::make( 'text', 'crb_facebook_url') ,
+        Field::make( 'textarea', 'crb_footer_text' )
+    ) );
 }
