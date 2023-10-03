f9fbbf523096faf6681088bfe2ae57fce183a332
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-03 19:08:06 +0200

    Add user messages

--- includes/templates/contact-form.php
+++ includes/templates/contact-form.php
@@ -1,3 +1,6 @@
+<div id="form_success" style="background:green; color:#fff;"></div>
+<div id="form_error" style="background:red; color:#fff;"></div>
+
 <form id="enquiry_form">
     <?php wp_nonce_field('wp_rest');?>
 
@@ -27,7 +30,15 @@
             $.ajax({
                 type: "POST",
                 url: "<?php echo get_rest_url(null, 'v1/contact-form/submit');?>",
-                data: form.serialize()
+                data: form.serialize(),
+                success: function(){
+                    form.hide();
+
+                    $("#form_success").html("Your message was sent.").fadeIn();
+                },
+                error: function() {
+                    $("#form_error").html("There was an error while submitting your form.").fadeIn();
+                }
             })
         });
     });
